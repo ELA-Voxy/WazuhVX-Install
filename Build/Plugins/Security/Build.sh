771a6b3fa0411d5edf3ef0fa68f8aea47e7319e2
ELA-Voxy <emeric.lai@voxy.fr>
2025-09-18 16:24:38 +0200

    Update Build.sh

--- Build/Plugins/Security/Build.sh
+++ Build/Plugins/Security/Build.sh
@@ -1,3 +1,25 @@
+cd ../../../../wazuh-dashboard-VX/plugins/
+rm -rf *
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+cd ..
+nvm install $(cat .nvmrc)
+nvm use $(cat .nvmrc)
+npm install -g yarn
+
+
+cd plugins/
+git clone -b hotfix-v4.12.0 https://github.com/ELA-Voxy/wazuh-security-dashboards-plugin-VX.git
+mv wazuh-security-dashboards-plugin-VX/ wazuh-security-dashboards-plugin/
+cd wazuh-security-dashboards-plugin/
+yarn
+yarn build
+
+
+
 cd ../
 git clone -b v4.12.0 https://github.com/ELA-Voxy/wazuh-dashboard-plugins-VX.git
 mv wazuh-dashboard-plugins-VX/ wazuh-dashboard-plugins/
@@ -17,6 +39,5 @@ yarn build
 
 cd ../../../packages
 zip -r -j ./security-package.zip ../wazuh-dashboard-VX/plugins/wazuh-security-dashboards-plugin/build/security-dashboards-2.*.*.0.zip
-zip -r -j ./wazuh-package.zip ../wazuh-dashboard-VX/plugins/wazuh-check-updates/build/wazuhCheckUpdates-2.*.*.zip ../wazuh-dashboard-VX/plugins/main/build/wazuh-2.*.*.zip ../wazuh-dashboard-VX/plugins/wazuh-core/build/wazuhCore-2.*.*.zip
-
-sudo bash ../WazuhVxInstall/Build/Rebuild.sh
+zip -r -j ./wazuh-package.zip ../wazuh-dashboard-VX/plugins/wazuh-check-updates/build/wazuhCheckUpdates-2.*.*.zip ../wazuh-dashboard-VX/plugins/main/build/>
+sudo bash ../WazuhVX-Install/Build/Rebuild.sh
