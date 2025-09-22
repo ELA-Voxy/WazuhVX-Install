493a49d88cc1c94c76b608e0925a4442d358f982
ELA-Voxy <emeric.lai@voxy.fr>
2025-09-22 23:17:22 +0200

    Update folder name

--- Manager/Managerinstall.sh
+++ Manager/Managerinstall.sh
@@ -1,4 +1,4 @@
 cd ~/
-git clone https://github.com/ELA-Voxy/wazuh-VX.git && cd wazuh/packages && git checkout v4.13.0
+git clone https://github.com/ELA-Voxy/wazuh-VX.git && cd wazuh-VX/packages && git checkout v4.13.0
 mkdir output
 ./generate_package.sh -s output/ -t manager -a amd64 -r 1 --system deb
