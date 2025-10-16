41f70e4541e45cfa2b2c047bc70e832896847253
ELA-VOXY <emeric.lai@voxy.fr>
2025-10-16 12:07:04 +0200

    (FIX) - Filebeat certificates

--- Filebeat/Filebeatinstall.sh
+++ Filebeat/Filebeatinstall.sh
@@ -16,9 +16,9 @@ curl -s https://packages.wazuh.com/4.x/filebeat/wazuh-filebeat-0.3.tar.gz | tar 
 
 # Certificates
 mkdir /etc/filebeat/certs
-tar -xf ./wazuh-certificates.tar -C /etc/filebeat/certs/ ./$NODE_NAME.pem ./$NODE_NAME-key.pem ./root-ca.pem
-mv -n /etc/filebeat/certs/$NODE_NAME.pem /etc/filebeat/certs/filebeat.pem
-mv -n /etc/filebeat/certs/$NODE_NAME-key.pem /etc/filebeat/certs/filebeat-key.pem
+tar -xf ./wazuh-certificates.tar -C /etc/filebeat/certs/ ./node-1.pem ./node-1-key.pem ./root-ca.pem
+mv -n /etc/filebeat/certs/node-1.pem /etc/filebeat/certs/filebeat.pem
+mv -n /etc/filebeat/certs/node-1-key.pem /etc/filebeat/certs/filebeat-key.pem
 chmod 500 /etc/filebeat/certs
 chmod 400 /etc/filebeat/certs/*
 chown -R root:root /etc/filebeat/certs
